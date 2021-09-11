66eb0f34cdc4cafca95d0a74256ca8740b4d7082
caarlosdamian <dapecarlos@gmail.com>
2021-09-11 10:11:56 -0500

    Feature: Add Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
+import Navbar from "./components/Navbar/Navbar";
 
-const Container = styled.div`
-  
-`;
+const Container = styled.div``;
 const App = () => {
-  return <Container>Hello world!</Container>;
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
 };
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,62 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Container = styled.div``
+const Container = styled.nav`
+  height: 100vh;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Left = styled.div`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Logo = styled.h1`
+  font-weight: 700;
+  text-decoration: underline crimson;
+`;
+const Menu = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+const MenuItem = styled.li`
+  margin-right: 30px;
+  font-size: 20px;
+  font-weight: bold;
+  color: gray;
+`;
+const Button = styled.button`
+  border: 2px solid white;
+  padding: 10px 15px;
+  background-color: crimson;
+  color: white;
+  font-weight: bold;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Logo>Agency</Logo>
+          <Menu>
+            <MenuItem>Home</MenuItem>
+            <MenuItem>Features</MenuItem>
+            <MenuItem>Services</MenuItem>
+            <MenuItem>Pricing</MenuItem>
+            <MenuItem>Contact</MenuItem>
+          </Menu>
+        </Left>
+        <Button>JOIN TODAY</Button>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
